55234caea554ed17189f46137c19130caaad00f4
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2025-11-17 12:15:19 -0300

    ajuste settings

--- instituto_caramelo/settings.py
+++ instituto_caramelo/settings.py
@@ -160,7 +160,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    ...
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
